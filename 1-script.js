20531da3d94810cad426714fedc98aa6962a90d0
Madn <madan.patakota@gmail.com>
2025-05-14 09:55:18 +0530

    Master branch created

---
+++ 1-script.js
@@ -0,0 +1,31 @@
+ //Comments we are going to use
+        // console.log("Hello world");
+        // console.log(123);
+        // console.log("Hello");
+
+        // document.write("Hello world<br>");
+        // document.write(123+"<br>");
+        // document.write("Hello<br>");
+
+        // document.write("Hello world");
+        // document.write(123);
+        // document.write("Hello");
+
+
+        // var customerID = 10;
+        // var customerID = 20;
+
+        // console.log(customerID);
+
+
+        // let customerID = 10;
+        //     customerID = 20;
+
+
+        const customerID = 10;
+       //       customerID = 20;
+
+        console.log(customerID);
+        //let customerID = 20;
+
+
