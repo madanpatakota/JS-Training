5cc6b0527c3f77a4a1766b68bab41d8f40693eca
Madn <madan.patakota@gmail.com>
2025-05-15 09:58:50 +0530

    2nd class

---
+++ 2-script.js
@@ -0,0 +1,88 @@
+let age = 50; //number
+
+
+let discount = 30; //number
+
+
+let averageValue = 30.05// number
+
+console.log(age);
+console.log(discount); 
+console.log(averageValue);
+
+
+//-----------------------------------
+//string data types
+//collection of charcters  a, b ,c $ , {
+
+// let technologyName = "Javascript"; //string
+// console.log(technologyName);
+
+// let technologyName = 'Javascript'; //string
+//  console.log(technologyName);
+
+
+
+
+// let technologyName = `Javascript`; //string
+//  console.log(technologyName);
+
+
+// let technologyPosition = 1;
+// let aboutJSTechnology = `Javascript is a ${technologyPosition} Programming Language`;
+
+// console.log(aboutJSTechnology );
+
+
+//----------------------------------------------------
+// let subjectName = "Javascript";
+// let Position    = 1;
+
+// let aboutJS = `${subjectName} is a Programming lan. Its is in NO.${Position} in IT Market.`
+// console.log(aboutJS)
+
+//---------------------------------------------
+
+
+//boolean data types
+
+// let isActive = true; //boolean
+// console.log(isActive);
+
+
+// let isAdmin = false; //boolean
+// console.log(isAdmin);
+
+
+//-------------------------------------------
+
+//undefined data types
+
+// let dateofBirth;
+// console.log(dateofBirth); //undefined
+
+//----------------------------------------------
+
+//null data types
+
+// let JoiningDate = null; // no value
+// console.log(JoiningDate); //null
+
+//----------------------------------------------
+
+//symbol data types
+
+let id = Symbol("john");  // unique id 
+console.log(id); //Symbol(123)
+
+
+
+
+
+
+
+
+
+
+
+
