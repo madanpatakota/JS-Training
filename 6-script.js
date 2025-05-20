1cadc419bdbc8ceae43e02c1f2a311a42294cea2
Madn <madan.patakota@gmail.com>
2025-05-20 08:31:42 +0530

    test

---
+++ 6-script.js
@@ -0,0 +1,76 @@
+
+// block1   -> group of statements
+//             Identity
+
+
+//function : is a kind of block 
+
+
+// Group 1    ->   
+
+// Group 2
+
+// Group 3
+
+
+//1. 
+
+//Declaration of the function
+function displayCustomerDetails() 
+{
+    let customerID = 1;
+    console.log(customerID);
+    //  window.alert(customerID);
+    //document.write(customerID);
+ 
+    let customerName = "RChandra";
+    console.log(customerName);
+}
+
+
+//let name = "RChandra"; // string
+
+displayCustomerDetails(); // Call the function
+
+
+
+
+
+
+
+
+
+// Work start -> while phase
+
+// 
+
+//Declaration
+function displayProductDetails(__________________,_____________________)
+{
+
+    let booksNames = ["Harry Potter series", "Pride and Prejudice", "The Waves"]; //strings
+
+    
+    let authorNames = ["J.K.Rowling", "Jane Austen", "Virginia Woolf"]; //strings
+    
+
+    let authorsYearsOfBirth = [1965, 1775, 1882]; // numbers
+   
+
+}
+
+
+displayProductDetails();
+
+
+//what is return in keyword?
+// parameter and argument
+
+
+
+
+let name = "asfasdfbasdbas;dla"; // string
+
+//console.log(name); // Output: RChandra
+
+//placeholder =>  "Pease enter your password" 
