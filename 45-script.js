1b617fe81dd387188b8ede2f92c879624242f56c
Madan.patakota <madan.patakota@gmail.com>
2025-06-11 09:59:35 +0530

    browserstorage

---
+++ 45-script.js
@@ -0,0 +1,32 @@
+//local storage
+
+// localStorage.setItem("userNameLoc" , "Mohan");
+
+// localStorage.setItem("userPassword" , "a^^^^^%$$#EDTYHU");
+
+
+// localStorage.clear();
+
+// localStorage.removeItem("userName");
+
+// localStorage.removeItem("userPassword");
+
+let username = localStorage.getItem("userNameLoc");
+document.write(`welcome ${username} `);
+
+
+
+//Session storage
+
+// sessionStorage.setItem("userName" , "Madan");
+// sessionStorage.setItem("userPassword" , "a^^^^^%$$#EDTYHU");
+
+
+//sessionStorage.clear();
+// sessionStorage.removeItem("userName");
+// sessionStorage.removeItem("userPassword");
+
+
+// let username = sessionStorage.getItem("userName");
+// document.writeln(`welcome ${username} `);
+
---
+++ modules/bank.js
@@ -0,0 +1,15 @@
+export const bankName = "HDFC Bank";
+
+
+export function openAccount(customerName){
+    return `${customerName}'s account opened in ${bankName}`;
+}
+
+
+export function getBalance(customerName , amount){
+    return `Balance for Account ${customerName} ${amount}`;
+}
+
+
+//openAccount("Madan")
+
